34728782596fab11dbb74909bc7cd435fa222dbd
Taras <klimarcuktaras@gmail.com>
2021-08-23 11:32:55 +0300

    app init

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`${PORT} running`);
+});
