ba5a77a7098bda2cf6f5ec6cc6d16e604375b9e6
Taras <klimarcuktaras@gmail.com>
2021-08-24 11:02:47 +0300

    add patients page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+import "./App.css";
+
+import { Patients } from "./pages";
 
 const App = () => {
   return (
-    <div>
-        APP
-    </div>
+    <Switch>
+      <Route exact path="/" component={Patients} />
+    </Switch>
   );
-}
+};
 
 export default App;
---
+++ client/src/pages/Patients/index.tsx
@@ -0,0 +1,105 @@
+import { FC, useState } from 'react';
+import { Result, Select, Table } from 'antd';
+import Search from 'antd/lib/input/Search';
+import { toJS } from 'mobx';
+import { observer } from 'mobx-react-lite';
+
+import useStore from '../../hooks/useStore';
+import { LoadingStatus } from '../../store/store';
+
+const { Option } = Select;
+
+const columns = [
+  {
+    title: 'patientID',
+    dataIndex: 'patientID',
+    key: 'patientID',
+  },
+  {
+    title: "Ім'я",
+    dataIndex: 'fullName',
+    key: 'fullName',
+  },
+  {
+    title: 'Номер телефону',
+    dataIndex: 'phone',
+    key: 'phone',
+  },
+];
+
+const Patients: FC = () => {
+  const { patientsStore } = useStore();
+
+  const isLoading = patientsStore.loadingStatus === LoadingStatus.LOADING;
+
+  const [type, setType] = useState<string>('name');
+
+  const onSearch = async (value: string) => {
+    if (!value.trim()) {
+      return false;
+    }
+
+    switch (type) {
+      case 'name':
+        await patientsStore.getByName(value);
+        break;
+      case 'phone':
+        await patientsStore.getByPhone(value);
+        break;
+      case 'id':
+        await patientsStore.getById(value);
+        break;
+    }
+  };
+
+  return (
+    <div>
+      <Search
+        placeholder="Пошук пацієнта"
+        onSearch={onSearch}
+        type={type === 'name' ? 'text' : 'number'}
+        loading={isLoading}
+        disabled={isLoading}
+        addonBefore={
+          <Select
+            style={{ width: 120, fontSize: 12 }}
+            onChange={(value: string) => setType(value)}
+            defaultValue="name"
+            className="select-after"
+          >
+            <Option style={{ fontSize: 12 }} value="name">
+              Ім'я
+            </Option>
+            <Option style={{ fontSize: 12 }} value="phone">
+              Номер телефону
+            </Option>
+            <Option style={{ fontSize: 12 }} value="id">
+              ID
+            </Option>
+          </Select>
+        }
+      />
+      {patientsStore.loadingStatus === LoadingStatus.ERROR ? (
+        <Result
+          status="500"
+          title="500"
+          subTitle="Sorry, something went wrong."
+        />
+      ) : (
+        <Table
+          style={{ marginTop: 20 }}
+          loading={isLoading}
+          dataSource={toJS(patientsStore.patients).map((p) => {
+            // @ts-ignore
+            p.key = p.uuid;
+            return p;
+          })}
+          columns={columns}
+          locale={{ emptyText: 'Введіть значення в пошуці' }}
+          pagination={false}
+        />
+      )}
+    </div>
+  );
+};
+export default observer(Patients);
